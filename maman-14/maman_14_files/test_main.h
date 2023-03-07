278ddc1c01b515e3b6e72358459589072f8651f0
omer-sade <108121786+omer-sade@users.noreply.github.com>
2023-03-07 11:44:51 +0200

    Update test_main.h

--- maman-14/maman_14_files/test_main.h
+++ maman-14/maman_14_files/test_main.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "data_struct/data_structure.c"
 #include "first_read/first_read.c"
@@ -11,22 +12,22 @@
 Reads origing file and "opens" all macros. Prepares the code for it to be readable by "reading
 file first time" function (located below) 
 */
-FILE *preAssembler(FILE *fp);
+void pre_assembler(FILE *fp, FILE *p_file_open_macros);
 
 /*
 Function that analyzes origin file for first time. Translates lines to binary if possible, else it'll
 save symbols (that will be translated to binary in the second read).
 Reads from "pfile", writes binary language to "p_outputFile"
 */
-void reading_file_first_time(FILE *pfile, Array *symbols_table, Array *insturctions, FILE *p_outputFile);
+void reading_file_first_time(Array *symbols_table, Array *insturctions, FILE *p_outputFile);
 
 /*
 Converts ALL remaining lines to binary. 
 Reads from "pfile", writes binary language to "p_outputFile"
 */
-void reading_file_second_time(FILE *pfile, Array *symbols_table, Array *insturctions, FILE *p_outputFile);
+void reading_file_second_time(Array *symbols_table, Array *insturctions, FILE *p_outputFile);
 
 /*
 adds all the 16 types of instructions to the instructions array (mov, not, jmp, etc..)
 */
-void add_data(Array instructions);
+void add_data(Array *instructions);
