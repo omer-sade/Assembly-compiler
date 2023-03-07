d2f9ae5ad8ec6d5b3829310b6cfb9e4f7b4a968f
omer-sade <108121786+omer-sade@users.noreply.github.com>
2023-03-07 19:48:49 +0200

    Update second_read.c
    changed instruction to "char array" instead of "Array"

--- maman-14/maman_14_files/second_read/second_read.c
+++ maman-14/maman_14_files/second_read/second_read.c
@@ -1,3 +1,3 @@
-void reading_file_second_time(FILE *pfile, Array *symbols_table, Array *insturctions, FILE *p_outputFile){
+void reading_file_second_time(Array *symbols_table, char instructions[][LINE_SIZE], FILE *p_outputFile){
     
 }
